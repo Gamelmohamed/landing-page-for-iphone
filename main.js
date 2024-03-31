6507c6986224000767d940e84f3ca0f130b0ba26
Gamelmohamed <138955241+Gamelmohamed@users.noreply.github.com>
2024-03-31 21:41:12 +0200

    landing page

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".iphone-img")
+let contaner = document.querySelector(".contaner")
+
+function phones(phone){
+    img.src = phone
+}
+
+function backgroundColor(color){
+    contaner.style.background = color
+}
