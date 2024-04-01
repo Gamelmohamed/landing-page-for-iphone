1de8af6fdaa27f0b8c344f43ab35cb134eef2611
Gamelmohamed <138955241+Gamelmohamed@users.noreply.github.com>
2024-04-01 21:08:02 +0200

    add start page

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let img = document.querySelector(".iphone-img")
 let contaner = document.querySelector(".contaner")
 let colorName = document.querySelector(".color-name h3")
+let page1 = document.getElementById("page1")
+let page2 = document.getElementById("page2")
+
+window.onload = function(){
+    page2.style.display = "none"
+}
 
 function changeColor(name){
     colorName.innerHTML = name
@@ -13,3 +19,13 @@ function phones(phone){
 function backgroundColor(color){
     contaner.style.background = color
 }
+
+function goToPage2(){
+page1.style.display="none"
+page2.style.display="block"
+}
+
+// function goToPage2(){
+//     page1.style.display="none"
+//     page2.style.display="block"
+//     }
