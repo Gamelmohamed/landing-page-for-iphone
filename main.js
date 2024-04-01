0b50e52358534ecb61ddcbf57926fd2ebd4430ec
Gamelmohamed <138955241+Gamelmohamed@users.noreply.github.com>
2024-04-01 20:17:31 +0200

    add color name

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let img = document.querySelector(".iphone-img")
 let contaner = document.querySelector(".contaner")
+let colorName = document.querySelector(".color-name h3")
+
+function changeColor(name){
+    colorName.innerHTML = name
+}
 
 function phones(phone){
     img.src = phone
